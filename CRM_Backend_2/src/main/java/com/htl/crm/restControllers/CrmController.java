32f0149c4e83624325d7ba3ea97988ea85092afd
Michi <michi.heckmann@gmail.com>
2018-01-23 12:58:50 +0100

    2nd Commit regarding 419135781d28f8e7d90b19e50744e29a646ca743

--- CRM_Backend_2/src/main/java/com/htl/crm/restControllers/CrmController.java
+++ CRM_Backend_2/src/main/java/com/htl/crm/restControllers/CrmController.java
@@ -1,27 +1,45 @@
 package com.htl.crm.restControllers;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.htl.crm.domain.AccessRight;
+import com.htl.crm.domain.ArPr;
 import com.htl.crm.repositories.AccessRightRepo;
 
 @RestController("/help/")
 @EnableWebMvc
 public class CrmController {
-	
+
 	@Autowired
 	private AccessRightRepo accessRightRepo;
 
 	@GetMapping(value = "/test/{id}", produces = "application/json")
-	public String test(@PathVariable int id) throws IOException {
-		
-		return "Dicks"+id;
+	public ResponseEntity<String> test(@PathVariable int id) throws IOException {
 
+		AccessRight ar = accessRightRepo.findOne(id);
+		// Wenn nicht gefunden ar == null
+		List<ArPr> list = (List<ArPr>) ar.getArPrs();
+		// Wenn kein Inhalt list.size() == 0
+		ar.setAccessRight("fhadshfasdo");
+		accessRightRepo.save(ar);
+		return ResponseEntity.status(HttpStatus.OK).body("Test" + id);
 	}
-	
+
+	@PostMapping(value = "/employee/addpersonaltutorial", produces = "application/json")
+	public ResponseEntity<String> addPersonalTutorial(@RequestBody String string) {
+
+		return ResponseEntity.status(HttpStatus.OK).body("Test" + string);
+	}
+
 }
