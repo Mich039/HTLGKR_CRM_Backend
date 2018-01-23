419135781d28f8e7d90b19e50744e29a646ca743
Michi <michi.heckmann@gmail.com>
2018-01-23 12:54:47 +0100

    In der ControllerKlasse einführung für die anderen erstellt.

--- CRM_Backend_2/src/main/java/com/htl/crm/restControllers/CrmController.java
+++ CRM_Backend_2/src/main/java/com/htl/crm/restControllers/CrmController.java
@@ -2,14 +2,20 @@ package com.htl.crm.restControllers;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.htl.crm.repositories.AccessRightRepo;
+
 @RestController("/help/")
 @EnableWebMvc
 public class CrmController {
+	
+	@Autowired
+	private AccessRightRepo accessRightRepo;
 
 	@GetMapping(value = "/test/{id}", produces = "application/json")
 	public String test(@PathVariable int id) throws IOException {
