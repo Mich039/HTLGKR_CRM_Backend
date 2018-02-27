d757b5168113819a927b66a61b6c2383be25c40b
Simon <Simon.Maurer1@gmx.at>
2018-02-27 13:48:06 +0100

    RestController - Profile
    Signed-off-by: Simon <Simon.Maurer1@gmx.at>

---
+++ CRM_Backend_2/src/main/java/com/htl/crm/transferclasses/Profile.java
@@ -0,0 +1,38 @@
+package com.htl.crm.transferclasses;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+
+public class Profile implements Serializable{
+	private String firstname;
+	private String lastname;
+	private LinkedList<String> todolist;
+	
+	public Profile() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	
+	public String getLastname() {
+		return lastname;
+	}
+	
+	public LinkedList<String> getTodolist() {
+		return todolist;
+	}
+	
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public void setTodolist(LinkedList<String> todolist) {
+		this.todolist = todolist;
+	}
+
+}
