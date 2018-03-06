8b1ff59ff03327772237564414c288207a93ee69
Michael Heckmann <michi.heckmann@gmail.com>
2018-03-06 14:45:37 +0100

    Fixed an Error when Role does not exist.

--- CRM_Backend_2/src/main/java/com/htl/crm/restControllers/RestAndreas.java
+++ CRM_Backend_2/src/main/java/com/htl/crm/restControllers/RestAndreas.java
@@ -15,6 +15,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import com.htl.crm.domain.Event;
 import com.htl.crm.domain.PData;
 import com.htl.crm.domain.PDatatype;
+import com.htl.crm.domain.PRole;
 import com.htl.crm.domain.Person;
 import com.htl.crm.repositories.AddressRepo;
 import com.htl.crm.repositories.EventRepo;
@@ -23,55 +24,62 @@ import com.htl.crm.repositories.PDatatypeRepo;
 import com.htl.crm.repositories.PRoleRepo;
 import com.htl.crm.repositories.PersonRepo;
 import com.htl.crm.transferclasses.AddContact;
-import com.htl.crm.transferclasses.Conversation;
+//import com.htl.crm.transferclasses.Conversation;
 import com.htl.crm.transferclasses.PersonData;
 
 @RestController("/andi-stuff/")
 @EnableWebMvc
 public class RestAndreas {
-	
-	
-		@Autowired
-		PersonRepo Persons;
-		@Autowired
-		PDatatypeRepo PDataTypes;
-		@Autowired
-		AddressRepo Addresses;
-		@Autowired
-		PRoleRepo PRoles;
-		@Autowired
-		EventRepo Events;
-		@Autowired
-		EventTypeRepo EventTypes;
-		
-		@PostMapping(value = "/addcontact", produces = "application/json")
-		public ResponseEntity<String> addPersonalTutorial(@RequestBody AddContact contact) {
-			Person p = new Person();
-			p.setPRole(PRoles.findByRoleText(contact.getRole()));
-			for(PersonData persondata : contact.getPersonData()) {
-				PData pdata = new PData();
-				PDatatype pdatatype = PDataTypes.findByType(persondata.getDatatype());
-				pdata.setPDatatype(pdatatype);
-				pdata.setValue(persondata.getValue());
-				p.addPData(pdata);
-			}
-			
-			Person pCreated = Persons.save(p);
-			return ResponseEntity.status(HttpStatus.CREATED).body(null);
+
+	@Autowired
+	PersonRepo Persons;
+	@Autowired
+	PDatatypeRepo PDataTypes;
+	@Autowired
+	AddressRepo Addresses;
+	@Autowired
+	PRoleRepo PRoles;
+	@Autowired
+	EventRepo Events;
+	@Autowired
+	EventTypeRepo EventTypes;
+
+	@PostMapping(value = "/addcontact", produces = "application/json")
+	public ResponseEntity<String> addPersonalTutorial(@RequestBody AddContact contact) {
+		Person p = new Person();
+		PRole role = PRoles.findByRoleText(contact.getRole());
+		if (role == null) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Specified Role does not exist!");
 		}
-		
-//		@GetMapping(value="/getconversatoinsofcompany", produces = "application/json")
-//		public ResponseEntity<String> getConversationsOfCompany(@PathVariable long id){
-//			Person p = Persons.findOne(id);
-//			
-//			LinkedList<Event> events = (LinkedList<Event>) Events.findByPerson(p);
-//			LinkedList<Conversation> convs = new LinkedList<>();
-//			for(Event e : events) {
-//				if(e.getEventType().getEventTypeId() == EventTypes.findByType("company_conversation").getEventTypeId())
-//					e.EventInfos();
-//					Conversation conv = new Conversation(e.Event, person_company, person_school, conversation_content)
-//			}
-//			return ResponseEntity.status(HttpStatus.OK).body(convs);
-//			
-//		}
+		p.setPRole(role);
+		for (PersonData persondata : contact.getPersonData()) {
+			PData pdata = new PData();
+			PDatatype pdatatype = PDataTypes.findByType(persondata.getDatatype());
+			pdata.setPDatatype(pdatatype);
+			pdata.setValue(persondata.getValue());
+			p.addPData(pdata);
+		}
+
+		Persons.save(p);
+		return ResponseEntity.status(HttpStatus.CREATED).body(null);
+	}
+
+	// @GetMapping(value="/getconversatoinsofcompany", produces =
+	// "application/json")
+	// public ResponseEntity<String> getConversationsOfCompany(@PathVariable long
+	// id){
+	// Person p = Persons.findOne(id);
+	//
+	// LinkedList<Event> events = (LinkedList<Event>) Events.findByPerson(p);
+	// LinkedList<Conversation> convs = new LinkedList<>();
+	// for(Event e : events) {
+	// if(e.getEventType().getEventTypeId() ==
+	// EventTypes.findByType("company_conversation").getEventTypeId())
+	// e.EventInfos();
+	// Conversation conv = new Conversation(e.Event, person_company, person_school,
+	// conversation_content)
+	// }
+	// return ResponseEntity.status(HttpStatus.OK).body(convs);
+	//
+	// }
 }
